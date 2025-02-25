06ee040ea80fae9129b87d1c42afd9da7e6521dc
jyoti140888 <140839074+jyoti140888@users.noreply.github.com>
2025-02-25 07:52:43 +0530

    Update Sample.java

--- src/test/java/basic/Sample.java
+++ src/test/java/basic/Sample.java
@@ -3,7 +3,7 @@ package basic;
 public class Sample {
 
 	public static void main(String[] args) {
-       System.out.println("hello");
+       System.out.println("hello hello hello welcom to qspider");
 	}
 
 }
